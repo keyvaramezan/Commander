ce624310c390a43dc56aacb99ee8996f8839b11d
unknown <keyvanramezan@gmail.com>
2021-02-02 13:30:15 +0330

    complete DataBaseContext Configuration and add to SqlDB

--- Models/Command.cs
+++ Models/Command.cs
@@ -8,8 +8,15 @@ namespace Models
         }
 
         public int Id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
         public string Line { get; set; }
+        
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.MaxLength(length: 250)]
         public string HowTo { get; set; }
+        
+        [System.ComponentModel.DataAnnotations.Required]
         public string Platform { get; set; }
     }
 }
